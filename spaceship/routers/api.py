498e6780e8d6fbfce82e27815c307033d70c3164
magicvegetable <maaagicvegetable@gmail.com>
2024-05-03 21:47:03 +0300

    l3-python:step-5-apline

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
-
 @router.get('')
 def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    ma = np.random.rand(10,10)
+    mb = np.random.rand(10,10)
+    product = np.multiply(ma, mb)
+    return {
+        'matrix_a': ma.tolist(),
+        'matrix_b': mb.tolist(),
+        'product': product.tolist()
+    }
